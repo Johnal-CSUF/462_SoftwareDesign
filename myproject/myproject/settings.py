0a80a80cb30125f020d1793c7162a8210cb1deb1
csudhtoro <32418263+csudhtoro@users.noreply.github.com>
2018-04-23 11:22:53 -0700

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'django.urls',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ 'C:\\Users\\Dana\\Desktop\\myproject\\myproject\\templates'],
+        'DIRS': [ 'C:\\Users\\dshorts\\Desktop\\myproject\\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
